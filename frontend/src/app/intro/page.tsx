aaf3ad09579ac27ef92f8bb2dd387e1463344bd5
jasper <latourjasper@gmail.com>
2026-01-08 23:21:03 +0100

    FEATURE: update image filenames to match casing in IntroPage component

--- frontend/src/app/intro/page.tsx
+++ frontend/src/app/intro/page.tsx
@@ -200,28 +200,28 @@ export default function IntroPage() {
                     onClick={() => { setLightboxSrc('/img/backgrounds/homepage-banner.jpg'); setLightboxOpen(true); }}
                   />
                   <img
-                    src="/img/intro-2-2025.jpg"
+                    src="/img/Intro-2-2025.jpg"
                     alt="polonaise"
                     className="w-full h-32 object-cover rounded-lg cursor-pointer"
                     onClick={() => { setLightboxSrc('/img/intro-2-2025.jpg'); setLightboxOpen(true); }}
                   />
                   <img
-                    src="/img/intro-3-2025.jpg"
+                    src="/img/Intro-3-2025.jpg"
                     alt="lasergame"
                     className="w-full h-32 object-cover rounded-lg cursor-pointer"
-                    onClick={() => { setLightboxSrc('/img/intro-3-2025.jpg'); setLightboxOpen(true); }}
+                    onClick={() => { setLightboxSrc('/img/Intro-3-2025.jpg'); setLightboxOpen(true); }}
                   />
                   <img
-                    src="/img/intro-4-2025.jpg"
+                    src="/img/Intro-4-2025.jpg"
                     alt="Groep"
                     className="w-full h-32 object-cover rounded-lg cursor-pointer"
-                    onClick={() => { setLightboxSrc('/img/intro-4-2025.jpg'); setLightboxOpen(true); }}
+                    onClick={() => { setLightboxSrc('/img/Intro-4-2025.jpg'); setLightboxOpen(true); }}
                   />
                    <img
-                    src="/img/intro2025.jpg"
+                    src="/img/Intro2025.jpg"
                     alt="Groep"
                     className="w-full h-32 object-cover rounded-lg cursor-pointer"
-                    onClick={() => { setLightboxSrc('/img/intro2025.jpg'); setLightboxOpen(true); }}
+                    onClick={() => { setLightboxSrc('/img/Intro2025.jpg'); setLightboxOpen(true); }}
                   />
                 </div>
               </div>
