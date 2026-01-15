6ff0f671068745929282caeded4617ec9c56b61f
RoanFontys <557605@student.fontys.nl>
2026-01-15 14:54:27 +0100

    fix(frontend): configure MSAL logging to reduce console spam

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -72,10 +72,10 @@ export default function RootLayout({
                 <link rel="preconnect" href="https://admin.salvemundi.nl" />
                 <link rel="dns-prefetch" href="https://admin.salvemundi.nl" />
 
-                
+
                 {/* Preload critical assets */}
                 <link rel="preload" href="/logo_purple.svg" as="image" />
-                
+
                 {/* Preload self-hosted Material Symbols font */}
                 <link rel="preload" href="/fonts/material-symbols/MaterialSymbolsOutlined.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
             </head>
