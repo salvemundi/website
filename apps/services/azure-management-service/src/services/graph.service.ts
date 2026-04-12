b8d7b9771aa4fc0701db93353c1320e8a8cd810c
RoanFontys <557605@student.fontys.nl>
2026-04-12 19:34:57 +0200

    fix: correct azure attributes mapping and resolve netId error

--- apps/services/azure-management-service/src/services/graph.service.ts
+++ apps/services/azure-management-service/src/services/graph.service.ts
@@ -1,5 +1,6 @@
 import { Client } from '@microsoft/microsoft-graph-client';
 import 'isomorphic-fetch';
+import crypto from 'crypto';
 
 export class GraphService {
     private static getClient(token: string): Client {
@@ -18,7 +19,7 @@ export class GraphService {
     static async addGroupMember(groupId: string, memberId: string, token: string) {
         const client = this.getClient(token);
         const memberUrl = `https://graph.microsoft.com/v1.0/directoryObjects/${memberId}`;
-        
+
         return await client.api(`/groups/${groupId}/members/$ref`)
             .post({
                 "@odata.id": memberUrl
@@ -42,7 +43,7 @@ export class GraphService {
     static async addGroupOwner(groupId: string, ownerId: string, token: string) {
         const client = this.getClient(token);
         const ownerUrl = `https://graph.microsoft.com/v1.0/directoryObjects/${ownerId}`;
-        
+
         return await client.api(`/groups/${groupId}/owners/$ref`)
             .post({
                 "@odata.id": ownerUrl
@@ -62,21 +63,23 @@ export class GraphService {
      * Creates a new user in Microsoft Entra ID.
      */
     static async createUser(
-        upn: string, 
-        firstName: string, 
-        lastName: string, 
+        upn: string,
+        firstName: string,
+        lastName: string,
         token: string,
         personalEmail: string,
         phoneNumber?: string,
-        dateOfBirth?: string
+        dateOfBirth?: string,
+        originalPaymentDate?: string,
+        membershipExpiry?: string
     ) {
         const client = this.getClient(token);
-        
+
         // Generate a cryptographically secure temporary password
         const randomPart = crypto.randomUUID().split('-')[0];
         const specialPart = crypto.randomUUID().split('-')[1].toUpperCase();
         const tempPassword = `SM-${randomPart}!${specialPart}#`;
-        
+
         const user: any = {
             accountEnabled: true,
             displayName: `${firstName} ${lastName}`,
@@ -90,23 +93,31 @@ export class GraphService {
             }
         };
 
+        if (phoneNumber) user.mobilePhone = phoneNumber;
+
         // Add Custom Security Attributes if provided
-        if (phoneNumber || dateOfBirth) {
+        if (dateOfBirth || originalPaymentDate || membershipExpiry) {
             user.customSecurityAttributes = {
                 SalveMundiLidmaatschap: {
                     "@odata.type": "#Microsoft.DirectoryServices.CustomSecurityAttributeValue",
                 }
             };
-            if (phoneNumber) user.customSecurityAttributes.SalveMundiLidmaatschap.Telefoon = phoneNumber;
             if (dateOfBirth) {
-                // Ensure date format is YYYYMMDD for Azure if needed, or stick to what SyncJob expects
                 const cleanDob = dateOfBirth.replace(/-/g, '');
                 user.customSecurityAttributes.SalveMundiLidmaatschap.Geboortedatum = cleanDob;
+            }
+            if (originalPaymentDate) {
+                const cleanPaidDate = originalPaymentDate.replace(/-/g, '');
+                user.customSecurityAttributes.SalveMundiLidmaatschap.OrigineleBetaalDatumStr = cleanPaidDate;
+            }
+            if (membershipExpiry) {
+                const cleanExpiry = membershipExpiry.replace(/-/g, '');
+                user.customSecurityAttributes.SalveMundiLidmaatschap.VerloopdatumStr = cleanExpiry;
             }
         }
 
         const result = await client.api('/users').post(user);
-        
+
         return {
             ...result,
             temporaryPassword: tempPassword
@@ -131,14 +142,14 @@ export class GraphService {
         const payload: any = {};
 
         if (data.displayName) payload.displayName = data.displayName;
+        if (data.phoneNumber) payload.mobilePhone = data.phoneNumber;
 
-        if (data.phoneNumber || data.dateOfBirth || data.membershipExpiry || data.originalPaymentDate) {
+        if (data.dateOfBirth || data.membershipExpiry || data.originalPaymentDate) {
             payload.customSecurityAttributes = {
                 SalveMundiLidmaatschap: {
                     "@odata.type": "#Microsoft.DirectoryServices.CustomSecurityAttributeValue",
                 }
             };
-            if (data.phoneNumber) payload.customSecurityAttributes.SalveMundiLidmaatschap.Telefoon = data.phoneNumber;
             if (data.dateOfBirth) {
                 const cleanDob = data.dateOfBirth.replace(/-/g, '');
                 payload.customSecurityAttributes.SalveMundiLidmaatschap.Geboortedatum = cleanDob;
