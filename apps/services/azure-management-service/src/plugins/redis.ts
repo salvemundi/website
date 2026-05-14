01eb10e4a674ee01decc2176ae25259ab2a31ba4
Roan <fontysroan@gmail.com>
2026-05-14 11:55:12 +0200

    Import fix

--- apps/services/azure-management-service/src/plugins/redis.ts
+++ apps/services/azure-management-service/src/plugins/redis.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyPluginAsync } from 'fastify';
 import fp from 'fastify-plugin';
 import { Redis } from 'ioredis';
 
@@ -8,17 +8,28 @@ declare module 'fastify' {
     }
 }
 
-export default fp(async (fastify: FastifyInstance) => {
+const redisPlugin: FastifyPluginAsync = async (fastify) => {
     const url = process.env.REDIS_URL || 'redis://v7-core-redis:6379';
+
     const client = new Redis(url, {
-        maxRetriesPerRequest: null
+        maxRetriesPerRequest: null,
+        retryStrategy(times) {
+            return Math.min(times * 50, 2000);
+        }
     });
 
-    client.on('error', (error: Error) => fastify.log.error(error, 'Redis Client Error'));
+    client.on('error', (error: Error) => {
+        fastify.log.error({ err: error }, 'Redis Client Error');
+    });
 
     fastify.decorate('redis', client);
 
     fastify.addHook('onClose', async (instance) => {
         await instance.redis.quit();
     });
+};
+
+export default fp(redisPlugin, {
+    name: 'custom-redis-plugin',
+    fastify: '5.x'
 });
